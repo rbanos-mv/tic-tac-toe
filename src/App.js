4ba02b9be919db1de2b32f411e70d7bf2568ecf8
Roberto A. Baños Alvarez <roberto.banos.mv@gmail.com>
2022-11-02 15:15:55 -0600

    Add winner detection

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 import PropTypes from 'prop-types';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
@@ -10,7 +10,40 @@ function App() {
   ];
   const randomPlayer = Date.now() % 2;
   const [player, setPlayer] = useState(players[randomPlayer]);
+  const [winner, setWinner] = useState(null);
   const [moves, setMoves] = useState([]);
+
+  function playerWon(filteredMoves) {
+    const xValues = [0, 0, 0];
+    const yValues = [0, 0, 0];
+    let diagonal = 0;
+    let diagonalReverse = 0;
+
+    filteredMoves.forEach((move) => {
+      xValues[move.coord[0]] += 1;
+      yValues[move.coord[1]] += 1;
+
+      const moveCoordJoin = move.coord.join();
+      if (['1,1', '2,2', '3,3'].includes(moveCoordJoin)) diagonal += 1;
+      if (['1,3', '2,2', '3,1'].includes(moveCoordJoin)) diagonalReverse += 1;
+    });
+
+    return (Object.values(xValues).find((val) => val === 3)
+            || Object.values(yValues).find((val) => val === 3)
+            || diagonal === 3 || diagonalReverse === 3);
+  }
+
+  useEffect(() => {
+    if (moves.length < 5) return;
+
+    const currentPlayer = moves[moves.length - 1].player;
+
+    const playerMoves = moves.filter((move) => move.player.name === currentPlayer.name);
+
+    if (playerWon(playerMoves)) {
+      setWinner({ name: currentPlayer.name, title: 'Winner' });
+    } else if (moves.length > 8) setWinner({ name: '', title: 'Tied match' });
+  }, [moves]);
 
   function addMove(coord) {
     const newMoves = [...moves];
@@ -55,10 +88,19 @@ function App() {
           ))}
         </div>
       </div>
-      <>
-        <h2>Current Player</h2>
-        <div>{player.name}</div>
-      </>
+      {winner
+        ? (
+          <>
+            <h2>{winner.title}</h2>
+            <div>{winner.name}</div>
+          </>
+        )
+        : (
+          <>
+            <h2>Current Player</h2>
+            <div>{player.name}</div>
+          </>
+        )}
     </div>
   );
 }
