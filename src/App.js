9051c3db5f794ae8dba9407121ba21e96ee01bcb
Roberto A. Baños Alvarez <roberto.banos.mv@gmail.com>
2022-11-02 13:58:16 -0600

    Show game board

--- src/App.js
+++ src/App.js
@@ -1,5 +1,40 @@
+import PropTypes from 'prop-types';
+import './App.css';
+
 function App() {
-  return <div className="App">Hello World</div>;
+  function Tile({ coord }) {
+    return (
+      <div className="tile">
+        {coord}
+      </div>
+    );
+  }
+
+  Tile.propTypes = {
+    coord: PropTypes.arrayOf(PropTypes.number).isRequired,
+  };
+
+  return (
+    <div className="App">
+      <div className="game-board">
+        <div className="tileRow">
+          {[[1, 1], [2, 1], [3, 1]].map((coord) => (
+            <Tile key={coord} coord={coord} />
+          ))}
+        </div>
+        <div className="tileRow">
+          {[[1, 2], [2, 2], [3, 2]].map((coord) => (
+            <Tile key={coord} coord={coord} />
+          ))}
+        </div>
+        <div className="tileRow">
+          {[[1, 3], [2, 3], [3, 3]].map((coord) => (
+            <Tile key={coord} coord={coord} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
